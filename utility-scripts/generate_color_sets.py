608daf4a1f5e25fff306021d0305a44fdc0c1843
Charles Vestal <charles@charles.pizza>
2025-06-06 06:28:48 +0200

    Fix path import for color set generator

--- utility-scripts/generate_color_sets.py
+++ utility-scripts/generate_color_sets.py
@@ -11,6 +11,12 @@ import os
 import shutil
 import tempfile
 import zipfile
+import sys
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).resolve().parents[1]
+if str(ROOT_DIR) not in sys.path:
+    sys.path.insert(0, str(ROOT_DIR))
 
 from core.list_msets_handler import list_msets_free
 from core.restore_handler import restore_ablbundle
