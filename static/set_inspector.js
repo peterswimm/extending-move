6a3e2c948dce5979253be0fb9a8aff7a53bd7146
Charles Vestal <charles@charles.pizza>
2025-06-14 15:20:41 +0200

    feat: allow hiding clip editor playback marker

--- static/set_inspector.js
+++ static/set_inspector.js
@@ -67,6 +67,7 @@ export function initSetInspector() {
     if (!piano.hasAttribute('xrange')) piano.xrange = region * ticksPerBeat;
     if (!piano.hasAttribute('markstart')) piano.markstart = loopStart * ticksPerBeat;
     if (!piano.hasAttribute('markend')) piano.markend = loopEnd * ticksPerBeat;
+    piano.showcursor = false;
     const { min, max } = notes.length
       ? { min: Math.min(...notes.map(n => n.noteNumber)),
           max: Math.max(...notes.map(n => n.noteNumber)) }
