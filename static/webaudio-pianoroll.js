7662168269b8b8cbe828a2b43af4e9bd4d220a0e
Charles Vestal <charles@charles.pizza>
2025-06-15 19:37:04 +0200

    Prevent browser menu when dismissing context menu

--- static/webaudio-pianoroll.js
+++ static/webaudio-pianoroll.js
@@ -1074,8 +1074,12 @@ customElements.define("webaudio-pianoroll", class Pianoroll extends HTMLElement 
                 window.removeEventListener('mousemove',this.bindpointermove,false);
                 window.removeEventListener('touchend',this.bindcancel,false);
                 window.removeEventListener('mouseup',this.bindcancel,false);
-                if(e.button==2||e.ctrlKey)
+                if(e.button==2||e.ctrlKey){
+                    ev.preventDefault();
+                    ev.stopPropagation();
+                    window.addEventListener("contextmenu",this.bindcontextmenu);
                     return;
+                }
             }
 
             if(this.menu.style.display=="block" && this.menu.contains(e.target)){
